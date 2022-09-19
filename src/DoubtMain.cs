e04bbb32801f8e8086724318c961c8fc0ba1c743
iubaldo <ianzachary.u@gmail.com>
2022-09-18 22:42:59 -0500

    fixed textures, adjusted item form transforms, item form functionality still broken

---
+++ src/DoubtMain.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+using Vintagestory.API.Common;
+using Vintagestory.API.Common.Entities;
+using Vintagestory.API.Client;
+using Vintagestory.API.Server;
+
+
+namespace doubt.src
+{
+    class DoubtMain : ModSystem
+    {
+        public override void Start(ICoreAPI api)
+        {
+            base.Start(api);
+
+            api.Logger.Warning("Loading Doubt...");
+
+
+        }
+    }
+}
